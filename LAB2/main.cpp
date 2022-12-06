1403b29452db6e5924b7e24d46d98903a4e955c4
Filiposki54 <filip.kierczynski@gmail.com>
2022-12-06 23:32:47 +0100

    correct

--- LAB2/main.cpp
+++ LAB2/main.cpp
@@ -32,24 +32,22 @@ int main() {
 
     auto Himmelblaus_f = [](std::pair<double, double> pair) {
         return  pow((pow(pair.first,2) + pair.second - 11),2) +
-                (pair.first + pow(pair.second,2 -7),2);
+                pow((pair.first + pow(pair.second,2) -7),2);
     };
 
-    auto McCormick_f = [](std::pair<double, double> pair) {
-        return  sin(pair.first + pair.second) +
-                pow((pair.first + pair.second),2) -
-                (1.5 * pair.first) + (2.5 * pair.second) + 1;
+    auto Booth_f = [](std::pair<double, double> pair) {
+        return  pow(pair.first + (2*pair.second)- 7,2) +
+                pow((2*pair.first) + pair.second -5,2);
     };
 
     auto ThreeHumpCamel_f = [](std::pair<double, double> pair) {
         return  (2*pow(pair.first,2)) -
-                (1.05 * pow(pair.first,4)) +
-                (pow(pair.first,6) / 6) +
-                (pair.first * pair.second) +
-                pow(pair.second,2);
+                (1.05*pow(pair.first,4)) +
+                (pow(pair.first,6)/6) +
+                (pair.first * pair.second) + pow(pair.second,2);
     };
     std::cout << "Results for Himmelblaus:\n";
-    for(int i = 0; i < 20; i++){                                                                                  // -5<=x, y<=5
+    for(int i = 0; i < 20; i++){
         auto start = high_resolution_clock::now();
         auto Himmelblaus = brute_force(Himmelblaus_f, coords_generator, 1000000, -5, 5);
         auto stop = high_resolution_clock::now();
@@ -60,20 +58,20 @@ int main() {
                   << std::endl;
         std::cout << " duration: " << duration.count() << std::endl;
     }
-    std::cout << "Results for McCormick:\n";
-    for(int i = 0; i < 20; i++){                                                                        // -1.5<=x<=4, -3<=y<=4
+    std::cout << "Results for Both:\n";
+    for(int i = 0; i < 20; i++){
         auto start = high_resolution_clock::now();
-        auto McCormick = brute_force(McCormick_f, coords_generator, 1000000, -1.5, 4);
+        auto Booth = brute_force(Booth_f, coords_generator, 1000000, -10, 10);
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop - start);
-        std::cout << " x = " << McCormick.first
-                  << " y = " << McCormick.second
-                  << " result: " << McCormick_f(McCormick)
+        std::cout << " x = " << Booth.first
+                  << " y = " << Booth.second
+                  << " result: " << Booth_f(Booth)
                   << std::endl;
         std::cout << "| duration: " << duration.count() << std::endl;
     }
     std::cout << "Results for ThreeHumpCamel:\n";
-    for(int i = 0; i < 20; i++){                                                                                  // -5<=x, y<=5
+    for(int i = 0; i < 20; i++){
         auto start = high_resolution_clock::now();
         auto ThreeHumpCamel = brute_force(ThreeHumpCamel_f, coords_generator, 1000000, -5, 5);
         auto stop = high_resolution_clock::now();
