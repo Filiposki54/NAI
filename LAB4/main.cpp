c2bbcfff6d307200142ee07dc461699111c8a5cb
Filiposki54 <filip.kierczynski@gmail.com>
2023-01-10 11:57:26 +0100

    Addet gray coding
    Implement gray code and decode for genetick algorithm

--- LAB4/main.cpp
+++ LAB4/main.cpp
@@ -14,6 +14,15 @@ auto booth = [](pair<double, double> pair) {
 random_device rd;
 mt19937 mt_generator(rd());
 
+vector<int> binaryToGray(vector<int> binary) {
+    vector<int> gray;
+    gray.push_back(binary[0]);
+    for (int i = 1; i < binary.size(); i++) {
+        gray.push_back(binary[i - 1] xor binary[i]);
+    }
+    return gray;
+}
+
 vector<vector<int>> generateGenoType(int size) {
     vector<vector<int>> genotype;
     vector<int> chromosomes;
@@ -26,31 +35,35 @@ vector<vector<int>> generateGenoType(int size) {
             chromosomes.push_back(uni(mt_generator));
         }
 
-        genotype.push_back(chromosomes);
+        genotype.push_back(binaryToGray(chromosomes));
     }
 
     return genotype;
 }
 
-pair<double, double> decodePhenotype(vector<int> chromosomes) {
+pair<double, double> decodePhenotype(vector<int> gray) {
     double x, y = 0.0;
-
-    for (int i = 0; i < chromosomes.size() / 2; i++) {
-        x = x * 2 + chromosomes[i];
+    vector<int> binary;
+    binary.push_back(gray[0]);
+    for (int i = 1; i < gray.size(); i++) {
+        binary.push_back(gray[i] xor binary[i-1]);
+    }
+    for (int i = 0; i < binary.size() / 2; i++) {
+        x = x * 2 + binary[i];
     }
 
-    for (int i = (int) chromosomes.size() / 2; i < chromosomes.size(); i++) {
-        y = y * 2 + chromosomes[i];
+    for (int i = (int) binary.size() / 2; i < binary.size(); i++) {
+        y = y * 2 + binary[i];
     }
 
-    x = x / pow(2.0, (chromosomes.size() / 2 - 4)) - 8;
-    y = y / pow(2.0, (chromosomes.size() / 2 - 4)) - 8;
+    x = x / pow(2.0, (binary.size() / 2 - 4)) - 8;
+    y = y / pow(2.0, (binary.size() / 2 - 4)) - 8;
 
     return {x, y};
 }
 
-double fitness(vector<int> chromosomes) {
-    return 1.0 / (1.0 + abs(booth(decodePhenotype(move(chromosomes)))));
+double fitness(vector<int> gray) {
+    return 1.0 / (1.0 + abs(booth(decodePhenotype(move(gray)))));
 }
 
 int main() {
